308289eb8d37a42ec2ad750e864cda6c742e32af
Me Me Mon <memethetmon@yahoo.com>
2016-04-18 16:35:58 -0700

    organize a bit

--- server/models/topic.js
+++ server/models/topic.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var Comments = new mongoose.Schema({
+	user: {type: Schema.Types.ObjectId, ref: 'User'},
+	comment: String
+});
+
+var Answers = new mongoose.Schema({
+	user: {type: Schema.Types.ObjectId, ref: 'User'},
+	answer: String,
+	upVote: {type: Number, default: 0},
+	downVote: {type: Number, default: 0},
+	comments: [Comments]
+});
+
 var TopicSchema = new mongoose.Schema({
 	// name: String,
 	user: {type: Schema.Types.ObjectId, ref: 'User'},
@@ -8,16 +21,7 @@ var TopicSchema = new mongoose.Schema({
 	topic: String,
 	description: String,
 	created_at: {type: Date, default: Date.now},
-	answers: [{
-		user: {type: Schema.Types.ObjectId, ref: 'User'},
-		answer: String,
-		upVote: {type: Number, default: 0},
-		downVote: {type: Number, default: 0},
-		comments: [{
-			user: {type: Schema.Types.ObjectId, ref: 'User'},
-			comment: String
-		}]
-	}]
+	answers: [Answers]
 });
 
 mongoose.model('Topic', TopicSchema);
